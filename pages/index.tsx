3a88c8b3c16aa4e2e81098a3f0e99ae9547a9cca
jdrew153 <jdrew153@gmail.com>
2022-10-14 13:57:36 -0400

    switched back to https

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
     <>
       <div>
           <ReactPlayer
-            url='//142.93.115.176:8000/live/test.flv'
+            url='https:://142.93.115.176:8000/live/test.flv'
             controls
 
           />
