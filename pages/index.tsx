57867f0084c0894c8794cba523b0754f92c7dda6
jdrew153 <jdrew153@gmail.com>
2022-10-14 08:48:13 -0400

    first push - streaming server

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import type { NextPage } from 'next';
+import dynamic from 'next/dynamic';
+
+
+
+
+const Home: NextPage = () => {
+
+  const ReactPlayer = dynamic(() => import('react-player'))
+
+ 
+
+  return (
+    <>
+      <div>
+          <ReactPlayer
+            url='http://142.93.115.176:8000/live/test.flv'
+            controls
+            
+          />
+        
+      </div>
+    </>
+  )
+}
+
+export default Home
